a82ffad4305080d8eb0431095d6f20e18e2d0e04
Herdantio <herdantio.sindhunata@binus.ac.id>
2021-10-12 12:53:25 +0700

    create todos page and configure route

---
+++ src/components/pages/TodosPage.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <h1>Hello World</h1>
+  </div>
+</template>
+
+<script lang="ts">
+import {Component, Vue} from 'vue-property-decorator'
+
+@Component
+export default class TodosPage extends Vue {}
+
+</script>
+
+<style scoped>
+
+</style>
